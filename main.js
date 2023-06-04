d5068bac3fcdec2ae7af48a2b3aa06ad36bdaa44
abrazo-malp <135549791+abrazo-malp@users.noreply.github.com>
2023-06-04 13:54:32 -0600

    commit inicial

---
+++ main.js
@@ -0,0 +1,9 @@
+
+setTimeout(function(){
+    document.getElementById("mensaje-cargando").innerText = "Enviado";
+}, 2300);
+setTimeout(function(){
+    alert("Recibiste un abrazo");
+    window.location.href = "envio.html";
+}, 3000);
+
